f36d3f00c2e657fddf28d49fc597a09ab26e078f
Gabriel Littman <gabe@dispatchr.co>
2016-05-31 13:51:55 -0700

    first checkin of ingest_map_service code, still need dependancies

---
+++ ingest_map_service/arcgis_ingest.js
@@ -0,0 +1,439 @@
+#!/bin/env node
+/*
+  @author Gabriel Littman
+  @fileoverview arcgis ingest command that will ingest transmission trees, lines, and project data
+*/
+
+var utils = require('dsp_tool/util');
+var esri_util = require("dsp_lib/esri/util");
+var http_get = esri_util.http_get;
+var assert = require('assert');
+var _ = require('underscore');
+var migrate_util = require('dsp_migrate/util');
+var BPromise = require("bluebird");
+var parse = require('csv-parse');
+var fs = require('fs');
+var TreeV3 = require("dsp_model/meteor_v3/tree");
+var PMD = require('dsp_model/meteor_v3/pmd');
+var Circuit = require('dsp_model/meteor_v3/circuit');
+var TreeStates = require('tree-status-codes');
+var geocoder = require('node-geocoder')(geocoderProvider, httpAdapter, extra);
+var Bpromise = require('bluebird');  
+
+
+var dsp_project = "transmission_2015";
+
+var detection_priorities = {
+  VC1c_URGENT: 1,
+  VC1c_AF: 2,
+  VC1c_MO: 3, 
+  VC2c_UH: 4, 
+  VC3c_UH: 5, 
+  VC1p_AF: 6, 
+  VC1p_MO: 7, 
+
+  VC2c: 8, 
+  VC3c: 9, 
+  VC2p: 10, 
+  VC3p: 11
+}; 
+
+/**
+ * Entry point of script
+ */
+function *run(){
+ var project = dsp_project.toUpperCase();
+ var host = "https://esri.dispatchr.co:6443";
+ var service_path = ["/arcgis/rest/services", project, "MapServer"].join("/");
+ var base_url = [host, service_path].join('/');
+ var base_params = {
+   f: "pjson"
+ };
+ var service = yield http_get(base_url, base_params);
+
+   // var layer_by_id = _.indexBy(service.layers, 'id')
+   for(var i = 0 ; i < service.layers.length; i++) {
+     var layer_group = service.layers[i];
+     if(layer_group.subLayerIds && layer_group.parentLayerId === -1) {
+       console.log("LAYER GROUP", layer_group.name, layer_group.id);
+       for(var j = 0; j < layer_group.subLayerIds.length; j++) {
+         var layer_id = layer_group.subLayerIds[j];
+         var layer = service.layers[layer_id];
+         if(layer.name.endsWith("TreeTops")) {
+           yield processLayer(base_url, layer);
+         }else if(layer.name.endsWith("Spans")) {
+           yield processSpansLayer(base_url, layer);
+         } 
+       }
+     }
+   }
+}
+
+/**
+ * @description queries the Map Service for all feature ids for all the features in the layer
+ * @param {String} base_url the url for ESRI Map Service
+ * @param {Object} layer layer object from a ESRI MapService 
+ */
+function *getFeatureIds(base_url, layer) {
+  var url = [base_url, layer.id, "query"].join("/");
+  var params = {
+    where:"1=1", 
+    f: "pjson", 
+    outFields: '*',
+    outSR: "4326",
+    returnIdsOnly: true
+  };
+  var ids = yield http_get(url, params);
+  ids = ids.objectIds || [];
+  return ids;
+}
+
+/**
+ * @description Process Spans Layer to create or update a Circuit info
+ * @param {String} base_url paramDescription
+ * @param {Object} layer layer object from a ESRI MapService 
+ */
+function *processSpansLayer(base_url, layer) {
+  var span_ids = yield getFeatureIds(base_url, layer);
+  var batch_size = 500;
+  var params = {
+    where:"1=1", 
+    f: "pjson", 
+    outFields: '*',
+    outSR: "4326",
+  };  
+  var url = [base_url, layer.id, "query"].join("/");
+  var line = {project: dsp_project, url: url};
+  for(var i = 0; i <= span_ids.length; i+=batch_size){ 
+    var ids = span_ids.slice(i, i+batch_size);
+    params.objectIds = ids.join(",");
+    var spans = yield http_get(url, params);    
+    _.each(spans.features, function(span){
+      line.name = sanitizeLineName(span.attributes.LINE_NAME);
+      line.voltage = span.attributes.VOLTAGE;
+      line.line_number = span.attributes.LINE_NBR;
+      line.division = span.attributes.DIVISION;
+      line.region = span.attributes.REGION;
+      line.length = line.length || 0;
+      line.length += span.attributes["SHAPE.STLength()"];
+    });
+  }
+  
+
+  var doc = yield Circuit.findOne({name: line.name});
+  if(!doc) {
+    yield Circuit.create(line);
+    console.log("Created Line", line.name);
+  } else {
+    if(line.url !== doc.url) {
+      doc.url = line.url;
+      yield doc.save();
+      console.log("Updated Line", line.name);
+    }
+  }
+}
+
+/**
+ * @description Process Tree Layer to create or update trees in dispatchr system
+ * @param {String} base_url paramDescription
+ * @param {Object} layer layer object from a ESRI MapService 
+ */
+function *processLayer(base_url, layer) {
+  var tree_ids = yield getFeatureIds(base_url, layer);
+  var tree_count = tree_ids.length;
+  console.log("Process Tree Layer", layer.name, layer.id, tree_count);           
+  
+  
+  var params = {
+    where:"1=1", 
+    f: "pjson", 
+    outFields: '*',
+    outSR: "4326"
+  };
+  
+  var batch_size = 500;
+  var url = [base_url, layer.id, "query"].join("/");
+  for(var i = 0; i <= tree_count; i+=batch_size){ 
+    var ids = tree_ids.slice(i, i+batch_size);
+    params.objectIds = ids.join(",");
+    var trees = yield http_get(url, params);    
+    assert(trees.features.length === batch_size || i+trees.features.length === tree_count);
+    yield processTrees(trees.features);
+  }  
+}
+
+/**
+ * @description Process a list of tree objects 
+ * @param {Array} trees An array of tree objects from the Map Service
+ */
+function *processTrees(trees) {
+  var qsi_ids = _.map(trees, function(tree){return tree.attributes.TREEID; });
+  var tree_docs = yield TreeV3.find({qsi_id: {$in: qsi_ids}});
+  console.log("DB Trees found", tree_docs.length);  
+  tree_docs = _.indexBy(tree_docs, "qsi_id");
+  for(var i = 0; i < trees.length; i++) {
+    var doc = tree_docs[trees[i].attributes.TREEID];
+    
+    if(doc) {
+      var address = {
+        streetNumber: doc.streetNumber,
+        streetName: doc.streetName,
+        city: doc.city,
+        state: doc.state,
+        zipcode: doc.postal,
+      };
+    }
+    
+
+    var tree = yield translateTree(trees[i], address);
+    if(shouldIngest(tree)){
+      if(!doc) {
+        doc = yield TreeV3.create(tree);
+        console.log("Created Tree", doc.qsi_id);
+      } else {
+        var doc_pri = detection_priorities[doc.pge_detection_type];
+        var tree_pri = detection_priorities[tree.pge_detection_type];
+        if(tree_pri < doc_pri) {
+          console.log("Updated Tree", doc.qsi_id);
+          //override particular values (don't override user entered values)
+          doc_pri.pge_detection_type = tree.pge_detection_type;
+          doc_pri.pge_pmd_num = tree.pge_detection_type;
+          doc_pri.span_name = tree.span_name;
+          doc_pri.circuit_name = tree.circuit_name;
+        }
+      }
+    }
+  }
+  // process.exit();
+}
+
+var pmd_projects = {};
+
+/**
+ * @description Finds a pmd resource or creates if not existing
+ * @param {String} pge_pmd_num PMD number  
+ */
+function *getPMD(pge_pmd_num) {
+  if(!pmd_projects[pge_pmd_num]) {
+    pmd_projects[pge_pmd_num] = yield PMD.findOne({pge_pmd_num: pge_pmd_num});
+  }
+  //still don't have it... lookin PMD.csv
+  if(!pmd_projects[pge_pmd_num]) {
+    pmd_projects[pge_pmd_num] = yield generatePMD(pge_pmd_num);
+  }
+  return pmd_projects[pge_pmd_num];
+}
+
+
+/**
+ * @description create a PMD object from PMD source csv file
+ */
+function *generatePMD(pge_pmd_num){
+  console.log("LOADING PMD FROM CSV", pge_pmd_num);
+  var pmds = yield readPMDCSV("tool/PMD.csv");
+  var pmd = pmds[pge_pmd_num];
+  return yield PMD.create(pmd);
+}
+
+const regions = {
+  "Sacramento": "North",
+  "Sierra": "North",
+  "North Bay": "North",
+  "North Coast": "North",
+  "North Valley": "North",
+  "East Bay":	"South",
+  "Mission ":	"South",
+  "Kern":	"South",
+  "Los Padres":	"South",
+  "Peninsula":	"South",
+  "San Jose":	"South",
+  "Central Coast":	"South",
+  "Fresno":	"South",
+  "Stockton":	"South",
+};
+
+
+/**
+ * @@description read pmd csv file into object array
+ * @param file_path file path to PMD.csv file
+ */
+function readPMDCSV(file_path){
+  return new BPromise(function(resolve, reject){
+    var parser = parse({columns: true});    
+    var input = fs.createReadStream(file_path);
+    input.pipe(parser);
+    var projects = {};
+
+    parser.on('readable', function(){
+      while(true){
+        var record = parser.read();
+        if( record ) {
+          var id = record["Project ID"].trim();
+          var name = record["Project Name"].trim();
+          var pi_complete_time = record["Complete Date Plan (PI)"].trim();
+          var tc_complete_time = record["Complete Date Plan (Trim)"].trim();
+          var division = record["Division Description"].trim();
+          var nerc = record.Bnerc.trim() === "1";
+          var type = record["Work Category Description"].trim();
+          if(projects[id]) {
+            if(projects[id].name !== name){
+              console.warn("Multiple Project Names for Project", id, name, projects[id]);
+            }
+          } else {
+            projects[id] = {
+              name: name,
+              pge_pmd_num: id,
+              division: division,
+              project: dsp_project,
+              plan_pi_complete: pi_complete_time,
+              plan_tc_complete: tc_complete_time,              
+              type: type,
+              nerc: nerc
+            };
+          }
+        } else {
+          break;
+        }
+      }
+    });
+    
+    parser.on('end', function(){
+      console.log("CSV Count:", _.size(projects));
+      resolve(projects);
+    });
+    parser.on('error', function(error){
+      reject(error);
+    });
+  });
+}
+
+/**
+ * @description description 
+ * @param {Object} tree 
+ * @returns {Boolean} true if the tree should be ingested
+ */
+function shouldIngest(tree) {
+  if(_.contains(["VC3c", "VC3p", "VC2p"], tree.pge_detection_type)) {
+    return false;
+  }
+  
+  return true;
+}
+
+/**
+ * @description transforms Map Service Tree into
+ */
+function *translateTree(tree, address) {
+  if(!address) {
+    address = yield getAddress(tree.geometry.x, tree.geometry.y);
+  }
+  tree = _.extend({}, tree.attributes, {geometry: tree.geometry}, address);
+  return yield migrate_util.applyMigrationSchema(migrate_schema, tree);
+}
+
+
+var geocoderProvider = 'google';
+var extra = {
+  apiKey: 'AIzaSyBUIRSmyeNNxy323BxyefxD2vnRV85Rfik', // for Mapquest, OpenCage, Google Premier
+  formatter: null   
+};
+var httpAdapter = 'https';
+function getAddress(x, y){
+  return new Bpromise(function(resolve,reject){
+    geocoder.reverse({lat: y, lon: x}, function (err, res) {
+      if (err) {
+        if(err[19] !== "Z"){
+          setTimeout(function () {
+              getAddress(location).then(function(result){
+                resolve(result);
+              });
+          }, 5000);
+        } else{
+          reject(err);
+        }
+      }else {
+        resolve(res[0]);
+      }
+    });
+  });
+}
+
+var migrate_schema = {
+  status : statusCode,  
+  height: "HEIGHT",
+  qsi_id: "TREEID",
+  circuit_name: function(tree){
+    return sanitizeLineName(tree.LINE_NAME);
+  },
+  pge_pmd_num: "PMD_NUM",
+  pge_detection_type: "Priority",
+  span_name: "SPAN_NAME",
+  location: function(tree){
+    return {type: "Point", coordinates: [tree.geometry.x, tree.geometry.y]};
+  },  
+
+  division: function*(tree) {
+    var pmd = yield getPMD(tree.PMD_NUM);
+    return pmd.division;
+  },
+  region: function*(tree) {
+    var pmd = yield getPMD(tree.PMD_NUM);
+    return regions[pmd.division];
+  },
+  
+  type: function() {return "tree"; },
+  project: function(){ return dsp_project; }, //TODO base of of ingestion project
+  streetNumber: "streetNumber",
+  streetName: "streetName",
+  city: "city",
+  state: "state",
+  zipcode: "postal",
+  
+  updated: function() { return new Date(); },  
+  created: function() { return new Date(); }  
+};
+
+/**
+ * @description cleans up line names to have standard format
+ * @param {String} str line name
+ * @returns {String} sanitized line name
+ */
+function sanitizeLineName(str){
+  str = str.trim();
+  str = str.replace(/[#\(\)\.]/g, "");
+  str = str.trim();
+  str = str.replace(/[\s-]/g, '_');
+  str = str.replace(/&/g, "_");
+  str = str.replace(/'/g, "_");    
+  str = str.replace(/_+/g, "_");
+  str = str.toUpperCase();
+  return str;
+}
+
+/**
+ * @description Calculates the status code string for a tree
+ * @param {Object} tree MapService Tree object
+ * @returns {String} tree status 
+ */
+function statusCode(tree){
+  var treeFlags = {};  
+  treeFlags.status = "left";
+  treeFlags.source = "lidar";
+  treeFlags.priority = "routine";
+  treeFlags.vc_codes = tree.Priority;
+  treeFlags.assigned = false;
+  treeFlags.dog = false;
+  treeFlags.irate_customer = false;
+  treeFlags.notify_customer = false;
+  treeFlags.ntw_needed = false;
+  treeFlags.access_issue = false;
+  treeFlags.vehicle_type = null;
+  treeFlags.environment = null;
+  return TreeStates.fetchStatusCode(treeFlags);
+}
+
+if (require.main === module) {
+  var baker = require('dsp_tool/baker');
+  utils.bakerGen(run, {default:true});  
+  baker.run();  
+}
---
+++ ingest_map_service/model/database.js
@@ -0,0 +1,96 @@
+var log = require('log4js').getLogger('['+__filename+']');
+var mongoose = require('mongoose');
+require("dsp_lib/starts_with");
+
+
+
+
+
+var connections = {};
+function getConnection(con_name, verbose) {
+  if(verbose === undefined) {
+    verbose = true;
+  }
+	if(!connections[con_name] && verbose) {
+		console.error("NO DATABASE FOUND", con_name);		
+	}
+	return connections[con_name];
+}
+
+function setConnection(con_name, con) {
+  connections[con_name] = con;
+  require("./connections")(con_name, con.connection);
+}
+
+
+  
+
+
+module.exports = function init_db(config) {
+    
+    var user_pass = "";
+    if(config.mongo_db_user) {
+      user_pass = config.mongo_db_user;
+      
+      if(config.mongo_db_pass) {
+        user_pass += ":"+config.mongo_db_pass;
+      }
+      user_pass += "@";
+    }
+    
+    var url = "mongodb://"+user_pass+config.mongo_db_host+":"+config.mongo_db_port+"/"+config.mongo_db_name;
+
+    var options = {
+        server: {socketOptions: {keepAlive: 1 }},
+        replset: {socketOptions: {keepAlive: 1}}
+      };
+
+    var connection;
+    var db = getConnection(config.name, false);
+    if(!db) {
+      var promise = new Promise(function(resolve, reject){      
+        connection = retryConnection(config.name, url, options, 0, 5, resolve, reject);
+      });      
+      
+      promise.then(function(connection){
+        var db = getConnection(config.name, false);
+        db.connection = connection;
+      });
+      
+      db = {
+        connection: connection,
+        connected: promise,
+        mongoose: mongoose
+      };      
+      setConnection(config.name, db);
+    }
+    
+
+    return db;
+    //mongoose is a singleton and you can get this connection at anytime if you require(mongoose)
+};
+
+function retryConnection(db_name, url, options, error_count, error_max, resolve, reject) {
+  log.info("Trying: Connecting to MongoDb(mongoose): ", url);
+  var db = mongoose.createConnection(url,options);
+  db.on('error', function(err) {
+    log.error("Error: MongoDb error", error_count, url, err.message, db.readyState);
+    if(db.readyState === 0) {//disconnected
+      error_count++;        
+      if(error_count < error_max) {
+        setTimeout(function(){
+          retryConnection(db_name, url, options, error_count+1, error_max, resolve, reject);
+        }, 1000*error_count);          
+      } else {
+        reject(err.message);
+      }
+    }
+  });
+
+  db.once('open', function() {
+    error_count = 0;
+    log.info("Success: Connected to MongoDb(mongoose) ", url);
+    resolve(db);
+  }); 
+  return db;    
+}
