158e66f49d7efe7eafbf50a20f7678f56fbb1389
Talal Arshad <talal7860@gmail.com>
2016-06-07 15:44:31 +0500

    DENG2-935 integrated pdf generation package

--- pdf_generation_service/resources/pdf/pdf-routes.js
+++ pdf_generation_service/resources/pdf/pdf-routes.js
@@ -1,11 +1,13 @@
 
 const express = require('express');
 const router = express.Router();
-const PdfModel = require('../pdf/pdf-resource');
+const PdfModel = require(CWD + '/resources/pdf/pdf-resource');
+const HtmlPdf = require('html-pdf');
+const fs = require('fs');
 
 
 router.post('/', (req, res, next) => {
-  var html = req.params.data;
+  var html = req.body.data;
   if (!html) {
     res.status(422).json
     ({
@@ -14,10 +16,20 @@ router.post('/', (req, res, next) => {
   } else {
     var pdf = new PdfModel();
     pdf.saveAsync()
-    .then(pdf => res.json({
+    .then(function(pdf) {
+      var options = { format: 'Letter' };
+      var dir = CWD + "/public/pdf";
+      if (!fs.existsSync(dir))
+        fs.mkdirSync(dir);
+      HtmlPdf.create(html, options)
+      .toFile(dir + '/' + pdf._id+ '.pdf', function(err, res) {
+        if (err) return console.log(err);
+        console.error(res); // { filename: '/app/businesscard.pdf' }
+      });
+      res.json({
         _id: pdf._id
       })
-    )
+    })
     .catch(err => next(err));
   }
 });
