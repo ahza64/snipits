4a52a8047e711b44030b4109e977ccf00c370f7d
Sarvar Muminov <sarvarmuminov@punch-agency.com>
2016-06-16 13:42:29 -0700

    Added pi_user_id into aggregation function, to avoid overlapping inspectors under the same locations
    Split run function into smaller ones for populating tree with pi and circuit, and location creation

--- export_vmd/extraction/index.js
+++ export_vmd/extraction/index.js
@@ -18,10 +18,10 @@ const TreeRecord = require("../work_packet/tree_record");
 function *run(params) {
   var startDate = Date.create(params.startDate);
   var endDate = Date.create(params.endDate);
-  
+
   console.log("from", startDate, "to", endDate);
   var includeExported = params.includeExported || false;
-  var query = { status: { $regex: /^[24]/} };
+  var query = { status: { $regex: /^[24]/}, pi_user_id: { $exists: true, $ne: null } };
 
   if(!includeExported) {
     query.exported = { $exists: false };
@@ -32,56 +32,54 @@ function *run(params) {
   }
   var date = JSON.parse(JSON.stringify(new Date())).replace(/:/g, '.');
 
-	query.span_name = { $ne: null };
-	query.city = { $ne: null, $exists: true };
+  query.span_name = { $ne: null };
+  query.city = { $ne: null, $exists: true };
   var cufs = yield CufModel.find({ work_type: 'tree_inspect' });
-  var aggregates = yield TreeModel.aggregate([{ $match: query }, { $group: { _id: '$pge_pmd_num', trees: { $push: "$$ROOT" }}}]).exec();
-  var circuits = yield CircuitModel.find();
+  var aggregates = yield TreeModel.aggregate([{ $match: query }, { $group: { _id: { pge_pmd_num: '$pge_pmd_num', pi_user_id: '$pi_user_id' }, trees: { $push: "$$ROOT" }}}]).exec();
+  var circuits = yield CircuitModel.find()
   var projects = yield PmdModel.find();
   var workorders = yield WorkorderModel.find({}, { uniq_id: 1, name: 1 });
 
   for(var i = 0; i < aggregates.length; i++) {
     var aggr = aggregates[i];
     console.log("aggr", i);
-    var pmd = _.find(projects, prj => prj.pge_pmd_num === aggr._id);
-    var packet = new WorkPacket();//"gabe@dispatchr.co");
-		var images = yield AssetModel.find({ _id: { $in: aggr.trees.map(tree => tree.image) } }, { data: 1 });
 
-    aggr.trees.forEach(tree => {
-			var uniq_id = tree.pge_pmd_num + tree.span_name + tree.streetNumber + tree.streetName + tree.city + tree.zipcode;
-			var workorder = _.find(workorders, wo => wo.uniq_id === uniq_id);
-			tree.pi = _.find(cufs, cuf => cuf._id === tree.pi_user_id.toString());
-			tree.circuit = _.find(circuits, circuit => circuit.name === tree.circuit_name);
-			tree.workorder_id = 'WO-T-' + workorder.name;
-    });
+    var pmd = _.find(projects, prj => prj.pge_pmd_num === aggr._id.pge_pmd_num);
+    var packet = new WorkPacket();
+    var images = yield AssetModel.find({ _id: { $in: aggr.trees.map(tree => tree.image) } }, { data: 1 });
+    var cuf = _.find(cufs, cuf => cuf._id === aggr._id.pi_user_id) || cufs[0];
+
+    aggr.trees.forEach(tree => preprareTree(tree, workorders, circuits, cuf));
 
     var locations = _.groupBy(aggr.trees, tree => tree.workorder_id);
     var location_names = Object.keys(locations);
 
-		location_names.forEach(name => {
-			var location = new TreeLocation();
-			locations[name].forEach(tree => {
-				var inspector = tree.pi;
-				var line = tree.circuit;
-				var image = _.find(images, img => img._id === tree.image) || {};
-				var record = new TreeRecord(tree, inspector, line, pmd, image.data);
-				location.addTree(record);
-			});
-			packet.addLocation(location);
-		});
+    location_names.forEach(name => packet.addLocation(createLocation(locations[name], pmd, images)));
 
-    
-    var filename = "vmd_export_"+pmd.pge_pmd_num+"_"+date+".xml";
+    var filename = "vmd_export_"+pmd.pge_pmd_num + "_" + cuf.uniq_id + "_" +"_"+date+".xml";
     fs.writeFile(filename, packet.toXML());
-
-    // var success = yield TreeModel.update({ _id: { $in: aggr.trees.map(tree => tree._id) }},
-    //     { $set: { exported: new Date().toISOString() }}, { multi: true });
-    // if(success){
-    //   console.log('Exported timestamp has been updated');
-    // } else {
-    //   console.error('Exported timestamp update failed');
-    // }
   }
+}
+
+function preprareTree(tree, workorders, circuits, cuf) {
+  var uniq_id = tree.pge_pmd_num + tree.span_name + tree.streetNumber + tree.streetName + tree.city + tree.zipcode;
+  var workorder = _.find(workorders, wo => wo.uniq_id === uniq_id);
+  tree.pi = cuf;
+  tree.circuit = _.find(circuits, circuit => circuit.name === tree.circuit_name);
+  tree.workorder_id = 'WO-T-' + workorder.name;
+  return tree;
+}
+
+function createLocation(trees, pmd, images) {
+  var location = new TreeLocation();
+  trees.forEach(tree => {
+    var inspector = tree.pi;
+    var line = tree.circuit;
+    var image = _.find(images, img => img._id === tree.image) || {};
+    var record = new TreeRecord(tree, inspector, line, pmd, image.data);
+    location.addTree(record);
+  });
+  return location;
 }
 
 //baker module
