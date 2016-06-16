588c2428b8727339c60d8d00976f93b5178d337b
Tejas <tejasrr19@gmail.com>
2016-06-16 13:29:20 -0700

    geonear city

--- migrate/migration/2016-06-13-county_migration/geonear.js
+++ migrate/migration/2016-06-13-county_migration/geonear.js
@@ -51,9 +51,51 @@ function geoNearCounty(){
   })
 }
 
+function geoNearCity(){
+  console.log('Trees with no county');
+  TREE.find({status: {$regex: /^[^06]/}, project: 'transmission_2015', city: {$exists: false}}).count().then(function(count){
+    console.log("Tree Count", count);
+  });
+
+  var trees = TREE.find({status: {$regex: /^[^06]/}, project: 'transmission_2015', city: {$exists: false}}).stream();
+
+  trees.on('data', function(doc){
+    if(!doc.city){
+      this.pause();
+      var self = this;
+      TREE.geoNear(doc.location, {minDistance: 0, maxDistance: 5500, spherical: true, query:{city:{$exists:true}}, limit:1}, function(err, nearbyTree){
+        if(err){
+          console.error(err);
+          self.resume();
+        }
+        else{
+          if(nearbyTree[0] === undefined){
+            console.log('------> No Nearby Tree for', doc._id);
+            self.resume();
+          }
+          else{
+            doc.city = nearbyTree[0].obj.city;
+            TREE.update({_id: doc._id}, {city: doc.city}, function(err){
+              if(err){
+                console.log(err);
+                self.resume();
+              }
+              else{
+                console.log(doc.city, doc._id);
+                self.resume();
+              }
+            });
+          }
+        }
+      });
+    }
+  })
+}
+
 //baker module
 if (require.main === module) {
   var baker = require("dsp_shared/lib/baker");
   baker.command(geoNearCounty);
+  baker.command(geoNearCity);
   baker.run();
 }
