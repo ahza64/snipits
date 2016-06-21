ba2a1293655843b3fe342afbab4cdac00adca236
xuchang chen <imxuchang@gmail.com>
2016-06-20 19:42:10 -0700

    DENG2-1093 :: add cuf company check

--- migrate/migration/2016-06-20-validation_suite/validate_suite.js
+++ migrate/migration/2016-06-20-validation_suite/validate_suite.js
@@ -3,7 +3,7 @@ var addressCheck = require('../2016-06-20-validate_address/validate_address');
 var trimCodeCheck = require('../2016-06-14-validate_trim_codes/validate_trim_code');
 var speciesCheck = require('../2016-06-16-validate_species/validateSpecies');
 var pmdCheck = require('../2016-06-14-pmds_with_letters/pmdsWithLetters');
-//var cufCheck = require('../2016-06-09_user_company_migration/add_company_to_cufs');
+var cufCheck = require('../2016-06-09_user_company_migration/add_company_to_cufs');
 
 function *runCheck () {
   var res = yield addressCheck.checkAll();
@@ -14,8 +14,8 @@ function *runCheck () {
   console.log('BAD SPECIES ===>', res);
   res = yield pmdCheck.getBadPmds();
   console.log('BAD PMD NUMBER ===>', res);
-  //res = yield cufCheck.cufsWithNoCompany();
-  //console.log('CHECKING CUF COMPANY ===>', res);
+  console.log('CUF without COMPANY ===>');
+  cufCheck.cufsWithNoCompany();
 }
 
 // baker module
