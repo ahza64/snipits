7bfd90989ce3e4cdefeabea8f56ee22b0053d6bd
Gabriel Littman <gabe@dispatchr.co>
2016-07-26 13:29:47 -0700

    fixing merge qsi logic

--- export_vmd/merge_qsi_id_csvs.js
+++ export_vmd/merge_qsi_id_csvs.js
@@ -1,27 +1,46 @@
 var utils = require('dsp_shared/lib/cmd_utils');
 require("sugar");
 var parse = require('csv-parse');
-var _ = require('underscore');
 var fs = require("fs");
 var assert = require("assert");
 var BPromise = require("bluebird");
 utils.connect(["meteor"]);
 var Tree = require('dsp_shared/database/model/tree');
-// var Export = require('dsp_shared/database/model/export');
+
+var db_stream = require('dsp_shared/database/stream');
+var Export = require('dsp_shared/database/model/export');
 // var generate = require('csv-generate');
 
-function *run(qsi_file_path, pge_file_path, output_file_path) {
-  var output = {};
+function *run(qsi_file_path, pge_file_path, data_file_path) {
+  var data = {};
   var count = 0;
   console.log("Loading Data");
   for(var record of streamCSV(pge_file_path)) {
     record = yield record;
     count++;
-    // assert(!output[record.ExternalTreeID], record.ExternalTreeID+" already exists.");
+    // assert(!data[record.ExternalTreeID], record.ExternalTreeID+" already exists.");
     console.log("Loaded Record", count, record.ExternalTreeID);
     record.UpdatedExternalTreeID = record.ExternalTreeID;
     record.UpdatedExternalLocID  = record.ExternalLocID;
-    output[record.ExternalTreeID] = record;
+    data[record.ExternalTreeID] = record;
+  }
+  
+  console.log("Checking exported Trees");
+  count = 0;  
+  for(var exp of db_stream(Export, {type: {$in: ["csv_export", "vmd_work_packet"]}})) {
+    count++;
+    exp = yield exp;
+    var exported_tree_id;
+    if(exp.type === "csv_export") {
+      exported_tree_id = exp.workorder_id;
+    } else {
+      exported_tree_id = exp.export_tree_id;
+    }
+    
+    console.error("Checking tree", count, exported_tree_id, exp.tree_id, exp.export_tree_id);
+    if(!data[exported_tree_id]) {
+      console.error("Missing expected exported tree", exported_tree_id, exp.tree_id, exp.export_tree_id);
+    }
   }
   
   console.log("Updating Data");
@@ -30,44 +49,62 @@ function *run(qsi_file_path, pge_file_path, output_file_path) {
     record = yield record;
     count++;
     var external_id = record.TREE_DETECTION_ID.toLowerCase();
-    if(output[external_id]) {
+    if(data[external_id]) {
       console.log("FOUND", count, record.TREEID, record.TREE_DETECTION_ID);
-      output[external_id].UpdatedExternalTreeID = record.TREEID;
-      output[external_id].UpdatedExternalLocID  = record.TREEID;
+      data[external_id].UpdatedExternalTreeID = record.TREEID;
+      data[external_id].UpdatedExternalLocID  = record.TREEID;
       // break;
     }
   }
   
-  console.log("Checking Data");
-  output = _.values(output);
-  for(var i = 0; i < output.length; i++) {
-    record = output[i];
-    var query={$or: [{qsi_id: record.UpdatedExternalTreeID}]};
 
-    if(record.UpdatedExternalTreeID.length === 24) {
-      query.$or.push({_id: record.UpdatedExternalTreeID});
-    }
-    var tree = yield Tree.find(query);
-    if(tree.length === 1) {
-      console.log("FOUND TREE", i, record.UpdatedExternalTreeID);
-    } else if(tree.length === 0) {
-      console.error("No Records Found:", record.UpdatedExternalTreeID);
-      record.UpdatedExternalTreeID = null;
-      record.UpdatedExternalLocID  = null;
-    } else if(tree.length > 1) {
-      assert(false, "Multiple Records Found: "+ record.UpdatedExternalTreeID);
+  
+  count = 0;
+  console.log("Checking Data");
+  for(var key in data) {
+    if(data.hasOwnProperty(key)) {
+      count++;
+      record = data[key];
+      var query={$or: [{qsi_id: record.UpdatedExternalTreeID}]};
+
+      if(record.UpdatedExternalTreeID.length === 24) {
+        query.$or.push({_id: record.UpdatedExternalTreeID});
+      }
+      var tree = yield Tree.find(query);
+      if(tree.length === 1) {
+        console.log("VALIDATED TREE", count, record.UpdatedExternalTreeID);
+      } else if(tree.length === 0) {
+        console.error("Can not find exported tree:", record.UpdatedExternalTreeID);
+        record.UpdatedExternalTreeID = null;
+        record.UpdatedExternalLocID  = null;
+      } else if(tree.length > 1) {
+        assert(false, "Multiple Records Found: "+ record.UpdatedExternalTreeID);
+      }
     }
   }
   
+
+  var output = [];
+  count = 0;
+  console.log("Generating Output");
+  for(record of streamCSV(pge_file_path)) {
+    record = yield record;
+    
+    record.UpdatedExternalTreeID = data[record.ExternalTreeID].UpdatedExternalTreeID;
+    record.UpdatedExternalLocID  = data[record.ExternalTreeID].UpdatedExternalLocID;
+    count++;
+    output.push(record);
+    // assert(!data[record.ExternalTreeID], record.ExternalTreeID+" already exists.");
+    console.log("Gerated Output", count, record.ExternalTreeID, record.UpdatedExternalTreeID);
+  }
   
-  fs.writeFile(output_file_path, utils.dumpAsCSV(output), function(err) {
+  
+  fs.writeFile(data_file_path, utils.dumpAsCSV(output), function(err) {
     if(err) {
         return console.log(err);
     }
-    console.log(output_file_path, "was saved!");
+    console.log(data_file_path, "was saved!");
   }); 
-  // var generator = generate({columns: _.keys(output[0]), length: 2});
-  // generator.pipe(process.stdout);
 }
 
 
