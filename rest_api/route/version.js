d6a4373a7811239d58905072282ca317056917b6
Tejas <tejasrr19@gmail.com>
2016-08-24 17:14:21 -0700

    DENG2-1711 :: App version endpoint

---
+++ rest_api/route/version.js
@@ -0,0 +1,24 @@
+/**
+* @fileOverview version for workr client
+*/
+if (require.main === module) {
+  var config = require('dsp_shared/config/config').get();
+  require('dsp_shared/database/database')(config.meteor);
+}
+var koa = require('koa');
+var router = require('koa-router')();
+var Client = require('dsp_shared/database/model/client');
+var app = koa();
+
+router.get('/client', function *() {
+  var client = this.request.query.name;
+  this.body = yield Client.find({name: client}).select({name:1, min_version:1, max_version:1, upgrade_url:1});
+  this.dsp_env.client_name = this.body[0].name;
+});
+
+app.use(router.routes());
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(3000);
+}
