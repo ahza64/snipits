fcba98ed7a458dda33d838970fc7dde2e09b93fd
Hasnain <hussu2001@gmail.com>
2016-08-25 17:35:34 -0700

    DENG2-1718

--- rest_api/route/package.js
+++ rest_api/route/package.js
@@ -9,9 +9,8 @@ var koa = require('koa');
 var router = require('koa-router')();
 var Cuf = require('dsp_shared/database/model/cufs');
 var Tree = require('dsp_shared/database/model/tree');
-var MapFeatures = require('dsp_shared/database/model/mapfeatures')
-var _ = require("underscore");
 var app = koa();
+var _ = require("underscore");
 
 //Things to test
 // cuf_id must exist
@@ -19,49 +18,32 @@ var app = koa();
 // Eventually need to get cuf_id from login
 
 router.get('/client/workr/n1/package', function*() {
-
-    var query = this.request.query;
-    var cufID = query.cuf_id;
-    var offset = parseInt(query.offset) || 0;
-    var length = parseInt(query.length) || 5000;
-
+    var cufID = this.request.query.cuf_id;
+    // var offset = this.request.query.offset;
+    // var length = this.request.query.length;
     var cuf = yield Cuf.findOne({
         _id: cufID
     });
 
-    var tree_ids = [];
-    var workorders = [];
-    var cufWOs = cuf.workorder;
-    var numberOfTrees = 0;
-    var skipped = 0;
+    //handle query.length query.offset into workorders
+    var workorders = cuf.workorder;
 
-    for (var i = 0; i < cufWOs.length && numberOfTrees < length; i++) {
-        var aWorkorder = cufWOs[i];
-        var tree_list = aWorkorder.tasks;
-        if (skipped > offset) {
-            workorders.push(aWorkorder);
-        }
-        for (var j = 0; j < tree_list.length && numberOfTrees < length; j++) {
-            if (skipped < offset) {
-                skipped++;
-                continue;
-            } else if (skipped === offset) {
-                workorders.push(aWorkorder);
-                skipped++;
-            }
-            tree_ids.push(tree_list[j]);
-            numberOfTrees++;
-        }
+    // workorders = workorders.slice(offset, length);
+
+    var tree_ids = [];
+    for (var i = 0; i < workorders.length; ++i) {
+        tree_ids = tree_ids.concat(workorders[i].tasks);
     }
 
+    //optimizaton - make one db request for trees
     var trees = yield Tree.find({_id: {$in: tree_ids}});
-
-    this.dsp_env.length = numberOfTrees;
-    this.dsp_env.offset = offset;
+    this.dsp_env.workorders = workorders.length;
+    this.dsp_env.trees = trees.length;
     this.body = {
       workorders: workorders,
       trees: _.indexBy(trees, tree => tree._id.toString())
     };
+
 });
 
 app.use(router.routes());
