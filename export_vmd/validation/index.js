791bbe1abf34f292246f486ab7002a75be92127b
Gabriel Littman <gabe@dispatchr.co>
2016-09-21 12:08:01 -0700

    fixing db connections

--- export_vmd/validation/index.js
+++ export_vmd/validation/index.js
@@ -1,12 +1,14 @@
 require('dsp_shared/config/config').get('meteor');
 const utils = require('dsp_shared/lib/cmd_utils');
+utils.connect(['meteor', 'postgres']);
+
 const validateMissingFields = require('./validate_address');
 const validateWorkorder = require('./validate_tree_workorder');
 const validateTcUserId = require('./validate_tc_user_id');
 const validateUserIssues = require('./validate_user_issues');
 
 require('sugar');
-utils.connect(['meteor']);
+
 
 function *run() {
   var fieldsExist = yield validateMissingFields.checkAll();
