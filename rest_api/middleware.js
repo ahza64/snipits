c04a8cf4e52e3a1e14adda04786ba8669ac5bcbf
xuchang chen <imxuchang@gmail.com>
2016-10-01 17:38:06 -0700

    DENG2-2223 :: refactor middlewares into one module

---
+++ rest_api/middleware.js
@@ -0,0 +1,67 @@
+var request_log = require('log4js').getLogger('request');
+
+module.exports = {
+  headerAccept: function *(next){
+    var url = this.request.url;
+    var header = this.request.header;
+    console.log('accept header:', header.accept);
+    if (url.endsWith('.jpeg') || url.endsWith('.jpg')) {
+      header.accept= "image/jpeg";
+      var sufix_len = url.endsWith('.jpeg') ? 5 : 4;
+      this.request.url = url.substring(0, url.length-sufix_len);
+    }
+    if(header.accept === '*/*' || !header.accept) {
+      header.accept = "application/json";
+    }
+    yield next;
+  },
+
+  envelope: function *(next){
+    this.dsp_env = {
+      request_id: this.id,
+      request_url: this.request.url,
+      host: this.request.header.host,
+      method: this.request.method
+    };
+    yield next;
+    if(this.request.header.accept === 'application/json') {
+      this.body = {
+        envelope: this.dsp_env,
+        data: this.body
+      };
+    }
+  },
+
+  auth: function*(next) {
+    if(this.isAuthenticated()) {
+      this.user = this.passport.user;
+      yield next;
+    } else {
+      if(this.request.header.accept === 'application/json'){
+        this.dsp_env.status = 400;
+      }
+      this.status = 400;
+      this.dsp_env.msg = 'Not Authenticated!!';
+    }
+  },
+
+  requestLog: function*(next){
+    var ip;
+    if(this.request.connection) {
+      ip = this.request.connection.remoteAddress;
+    }
+
+    var log_me = {
+      method: this.method,
+      host: this.request.host,
+      url: this.originalUrl,
+      body: this.request.body,
+      user: yield Promise.resolve(this.user),
+      user_ip: ip,
+      "user-agent": this.request.header['user-agent']
+    };
+    request_log.info(JSON.stringify(log_me));
+
+    yield next;
+  }
+};
