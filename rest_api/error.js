a7218b74a238ebfb0392bd73e8718d8f8bb35385
Tejas <tejasrr19@gmail.com>
2016-10-11 11:36:04 -0700

    DENG2-2216 :: Some cleanup

--- rest_api/error.js
+++ rest_api/error.js
@@ -1,6 +1,7 @@
 const deepFreeze = require('deep-freeze-strict');
 
-const ERROR_RESOURCE = deepFreeze({
+const STATUS_RESOURCE = deepFreeze({  
+  LOGIN_ERROR: { status: 401, message: 'NOT AUTHENTICATED!!'},
   DUPLICATE_TREE: { status: 409, message: 'Tree already exists at this location'},
   GEOCODE_ERROR: { status: 201, message: 'No Address found at location'},
   NOT_ADDED: {status: 400, message: 'not added'},
@@ -12,16 +13,16 @@ const ERROR_RESOURCE = deepFreeze({
 function handleError(context, error, message) {
   console.log('EXCEPTION: ', error, context);
   if(typeof error  === 'string') {
-    error = ERROR_RESOURCE[error];
+    error = STATUS_RESOURCE[error];
   }
-  context.dsp_env.msg = message || error.message;
+  context.dsp_env.error_msg = message || error.message;
   context.dsp_env.status = error.status;
   context.status = error.status;
 }
 
 function *middleware(next) {
   try {
-    this.errors = ERROR_RESOURCE;
+    this.errors = STATUS_RESOURCE;
     this.setError = function(error, message){
       handleError(this, error, message);
     };
