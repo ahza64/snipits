f53ab4f2044358f6ae4d738a6fb08f1beed7d1b6
Gabriel Littman <gabe@dispatchr.co>
2016-10-24 17:36:58 -0700

    adding migration for start time to complete time on tirmmed trees

---
+++ migrate/migration/2016-10-24-set_pi_time_on_trimmed/set_pi_complete_time.js
@@ -0,0 +1,43 @@
+#!/bin/env node
+/*
+   @author gabe@dispatchr.com
+   @fileoverview sets source to mobile for all tree histories that do not have a source attribute.
+*/
+var utils = require('dsp_shared/lib/cmd_utils');
+utils.connect(['meteor', 'postgres']);
+var Tree = require("dsp_shared/database/model/tree");
+var stream = require("dsp_shared/database/stream");
+var TreeHistory = require("dsp_shared/database/model/tree-history");
+
+function *run(fix){
+  console.log("Setting tree history source");
+  var query = {pi_complete_time: null, pi_start_time: {$ne: null}, status: /^5/};
+  var count = yield Tree.find(query).count();
+  console.log("Fixing Trees", count);
+  var tree_count = 0;
+  for(var tree of stream(Tree, query)) {
+    tree = yield tree;
+    if(tree) {
+      console.log("Setting compete date", tree.status, tree.pi_start_time, tree.pi_complete_time);
+      tree_count++;
+      
+      if(fix) {
+        var old_tree = utils.toJSON(tree);
+        tree.pi_complete_time = tree.pi_start_time;
+        yield tree.save();
+        var mig_user = {_id: 'fix_tree_soruce', type:"Migration"};
+        yield TreeHistory.recordTreeHistory(old_tree, tree, mig_user);
+      }
+    }
+  }
+}
+
+
+if (require.main === module) {
+  var baker = require('dsp_shared/lib/baker');
+  utils.bakerGen(run, {default:true});
+  baker.run();
+}
+
+module.exports = run;
+
