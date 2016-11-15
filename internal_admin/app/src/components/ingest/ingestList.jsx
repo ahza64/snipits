9fbb4ca041c0375d1cb0e332f823759cc43e7dad
xuchang chen <imxuchang@gmail.com>
2016-11-15 13:38:59 -0800

    DENG2-2542 :: simple filter for search purpose

--- internal_admin/app/src/components/ingest/ingestList.jsx
+++ internal_admin/app/src/components/ingest/ingestList.jsx
@@ -12,6 +12,7 @@ import ActionMenu from './actionMenu';
 // Styles
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
+import TextField from 'material-ui/TextField';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 
 export default class IngestList extends IngestLib {
@@ -19,11 +20,14 @@ export default class IngestList extends IngestLib {
     super();
 
     this.state = {
-      ingestions: []
+      ingestions: [],
+      search: '',
     };
 
     this.getIngestionStatus = this.getIngestionStatus.bind(this);
     this.resetIngestionList = this.resetIngestionList.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.isMatchSearchRegex = this.isMatchSearchRegex.bind(this);
   }
 
   componentWillMount() {
@@ -40,42 +44,65 @@ export default class IngestList extends IngestLib {
     return ingested ? 'INGESTED' : 'NOT INGESTED';
   }
 
+  handleSearch(event, value) {
+    this.setState({ search: value }); 
+  }
+
+  isMatchSearchRegex(ingestion) {
+    var search = this.state.search;
+    var regexp = new RegExp('.' + search + '.', 'i');
+    return ingestion.fileName.match(regexp);
+  }
+
   render() {
     return (
       <div>
-        <Table selectable={ false }>
-          <TableHeader displaySelectAll={ false } adjustForCheckbox={ false }>
-            <TableRow>
-              <TableHeaderColumn>ID</TableHeaderColumn>
-              <TableHeaderColumn>File</TableHeaderColumn>
-              <TableHeaderColumn>Last Modified Time</TableHeaderColumn>
-              <TableHeaderColumn>Status</TableHeaderColumn>
-              <TableHeaderColumn>Action</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody displayRowCheckbox={ false } selectable={ false }>
-            { 
-              this.state.ingestions.map((ingestion, idx) => {
-                return (
-                  <TableRow key={ idx }>
-                    <TableRowColumn>{ idx }</TableRowColumn>
-                    <TableRowColumn>{ ingestion.fileName }</TableRowColumn>
-                    <TableRowColumn>{ ingestion.updatedAt }</TableRowColumn>
-                    <TableRowColumn>{ this.getIngestionStatus(ingestion.ingested) }</TableRowColumn>
-                    <TableRowColumn>
-                      <ActionMenu
-                        ingestions={ this.state.ingestions }
-                        idx={ idx }
-                        ingestion={ ingestion }
-                        resetIngestionList={ this.resetIngestionList }
-                      />
-                    </TableRowColumn>
-                  </TableRow>
-                );
-              })
-            }
-          </TableBody>
-        </Table>
+        <Row>
+          <TextField
+            hintText='Search Ingestions ... '
+            fullWidth={true}
+            value={ this.state.search }
+            onChange={ this.handleSearch }
+          />
+        </Row>
+        <Row>
+          <Table selectable={ false }>
+            <TableHeader displaySelectAll={ false } adjustForCheckbox={ false }>
+              <TableRow>
+                <TableHeaderColumn>ID</TableHeaderColumn>
+                <TableHeaderColumn>File</TableHeaderColumn>
+                <TableHeaderColumn>Last Modified Time</TableHeaderColumn>
+                <TableHeaderColumn>Status</TableHeaderColumn>
+                <TableHeaderColumn>Action</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={ false } selectable={ false }>
+              { 
+                this.state.ingestions.map((ingestion, idx) => {
+                  if (!this.isMatchSearchRegex(ingestion)) {
+                    return;
+                  }
+                  return (
+                    <TableRow key={ idx }>
+                      <TableRowColumn>{ idx }</TableRowColumn>
+                      <TableRowColumn>{ ingestion.fileName }</TableRowColumn>
+                      <TableRowColumn>{ ingestion.updatedAt }</TableRowColumn>
+                      <TableRowColumn>{ this.getIngestionStatus(ingestion.ingested) }</TableRowColumn>
+                      <TableRowColumn>
+                        <ActionMenu
+                          ingestions={ this.state.ingestions }
+                          idx={ idx }
+                          ingestion={ ingestion }
+                          resetIngestionList={ this.resetIngestionList }
+                        />
+                      </TableRowColumn>
+                    </TableRow>
+                  );
+                })
+              }
+            </TableBody>
+          </Table>
+        </Row>
       </div>
     );
   }
