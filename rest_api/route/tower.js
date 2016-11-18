d6ae7c9de39fa1ad17c48bec3e157c7d325b98ba
Tejas <tejasrr19@gmail.com>
2016-11-18 13:03:47 -0800

    DENG2-2629 :: Create general layer route for spans and towers

--- rest_api/route/tower.js
+++ rest_api/route/tower.js
@@ -15,10 +15,11 @@ var esriConfig =  require('dsp_shared/conf.d/config').esriConfig;
 const Server = require('dsp_shared/lib/gis/esri/arcserver/index');
 const Token = require('dsp_shared/lib/gis/esri/arcserver/token');
 
-router.get('/workr/tower', function*(){
+router.get('/workr/layer/:type', function*(){
 
   try {
     var userId = this.req.user._id;
+    var type = this.params.type;
     var trees = [];
     var lines = [];
     var lats = [];
@@ -32,12 +33,10 @@ router.get('/workr/tower', function*(){
     trees = _.uniq(trees);
 
     var treeObjs = yield Tree.find({_id: {$in: trees}});
-    var multipoints = [];
     _.each(treeObjs, tree => {
       lines = lines.concat(tree.circuit_name);
       lats = lats.concat(tree.location.coordinates[0]);
       longs = longs.concat(tree.location.coordinates[1]);
-      multipoints.push(tree.location.coordinates);
     });
     lines = _.uniq(lines);
     //lines = ['BRIGHTON_DAVIS'];
@@ -54,9 +53,9 @@ router.get('/workr/tower', function*(){
     var server = new Server();
     var token =  new Token(esriConfig.serverUrl, esriConfig.username, esriConfig.password);
     server.setToken(token);
-    var folder = yield server.getFolder(esriConfig.towerLayer.folder);
-    var service = yield folder.getService(esriConfig.towerLayer.service);
-    var layer = yield service.getLayer(esriConfig.towerLayer.layer);
+    var folder = yield server.getFolder(esriConfig[type].folder);
+    var service = yield folder.getService(esriConfig[type].service);
+    var layer = yield service.getLayer(esriConfig[type].layer);
     var whereQuery = "";
 
     //For all lines, look up line number and generate the where clause
@@ -69,7 +68,6 @@ router.get('/workr/tower', function*(){
         whereQuery = whereQuery + "LINE_NBR='"+ line_number + "' or ";
       }
     }
-    console.log('WHERE QUERY:', whereQuery);
     var params = {
       where: whereQuery,
       geometryType: 'esriGeometryEnvelope',
@@ -78,38 +76,23 @@ router.get('/workr/tower', function*(){
       returnGeometry : true,
       inSR: JSON.stringify({"wkid" : 4326}),
       f: "pjson",
-      outFields: 'LINE_NAME, STR_NUM',
+      outFields: esriConfig[type].outFields,
       outSR: JSON.stringify({"wkid" : 4326})
     };
-    var towers = yield layer.query(params);
 
-    towers = towers.features.map( tower => {
-      return {
-        x: tower.geometry.x,
-        y: tower.geometry.y,
-        STR_NUM: tower.attributes.STR_NUM,
-        LINE_NAME: tower.attributes.LINE_NAME
-      }
-    });
+    //query respective layer
+    var data = yield layer.query(params);
+
     this.body = {};
     this.body.lines = lines;
     this.body.extent = extent;
     this.body.params = params;
-    this.body.count = towers.length;
-    this.body.towers = towers;
+    this.body.count = data.features.length;
+    this.body[type] = data.features;
   } catch(e) {
     this.dsp_env.error_message = e.message;
   }
-
 });
 
 app.use(router.routes());
 module.exports = app;
-
-// //This is runnable as a stand alone server
-// if(require.main === module) {
-//   app.use(bodyParser());
-//   var logger = require('koa-logger');
-//   app.use(logger());
-//   app.listen(3000);
-// }
