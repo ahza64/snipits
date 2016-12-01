03e20a7d4c475bb3402b625d3912f8c5d42a5580
Gabriel Littman <gabe@dispatchr.co>
2016-11-30 17:04:41 -0800

    fixes for date issues

--- export_vmd/index.js
+++ export_vmd/index.js
@@ -1,3 +1,4 @@
+var config = require('dsp_shared/config/config').get();
 require('sugar');
 const utils = require('dsp_shared/lib/cmd_utils');
 var validation = require("./validation");
@@ -5,6 +6,8 @@ var extraction = require("./extraction");
 var mark_exported = require("./mark_exported");
 var spawn = require('child-process-promise').spawn;
 var assert = require('assert');
+var moment = require('moment');
+var path = require('path');
 
 var s3 = require('dsp_shared/aws/s3');
 var fs = require("fs");
@@ -12,32 +15,32 @@ var bluebird = require('bluebird');
 fs.readdirAsync = bluebird.promisify(fs.readdir);
 fs.openAsync = bluebird.promisify(fs.open);
 
-var S3_BUCKET = "prod.pge.ftp.export.vmd"; //"dev.pge.ftp.export.vmd"; 
-var DISTRIBUTION_LIST = "vmd_export";
+var S3_BUCKET = config.pge_vmd_export.bucket;
+var DISTRIBUTION_LIST = config.pge_vmd_export.distribution_list;
 
 var gen_email = require('dsp_email/create_mail');
 
 function *run(end_date, start_date) {
   end_date = santitizeDate(end_date || 'last saturday');
-  start_date = santitizeDate(start_date || "2015-01-01T00:00:00.000Z");
+  start_date = santitizeDate(start_date || "2015-01-01T00:00:00.000Z");  
   
-  var file_date =  end_date.format("{yyyy}-{MM}-{dd}_{HH}.{mm}.{ss}.000Z");
-  
-  console.log(start_date, end_date, file_date);
+  console.log(start_date, end_date);
   yield validation.fixes();
 
   // work packet export
-  yield extraction.run(start_date, end_date, false, undefined, "wp_"+file_date, undefined, false, false, true);//workpacket
-  yield tar(file_date, 'wp');
+  var dir = export_dir(end_date, "wp");
+  yield extraction.run(start_date.toDate(), end_date.toDate(), false, undefined, path.basename(dir), undefined, false, false, true);
+  yield tar(end_date, 'wp');
   yield upload(end_date, 'wp');
-  yield mark_exported.run(`vmd_export/wp_${file_date}`, end_date);
+  yield mark_exported.run(dir, end_date);
 
 
   //work complete export
-  yield extraction.run(start_date, end_date, false, undefined, "wc_"+file_date, undefined, true, false, true);//workpacket
-  yield tar(file_date, 'wc');
+  dir = export_dir(end_date, "wc");
+  yield extraction.run(start_date.toDate(), end_date.toDate(), false, undefined, path.basename(dir), undefined, true, false, true);
+  yield tar(end_date, 'wc');
   yield upload(end_date, 'wc');
-  yield mark_exported.run(`vmd_export/wc_${file_date}`, end_date);
+  yield mark_exported.run(dir, end_date);
 
   yield email(end_date); 
 }
@@ -49,23 +52,32 @@ function santitizeDate(date) {
     if(!m) { // no timezone
       date = Date.create(date);
       var local_offset = date.getTimezoneOffset();
-      date.addMinutes(-1*local_offset);
+      date.addMinutes(-1*local_offset);      
     } else {
       date = Date.create(date);
     }
   }
-
-  return date.utc();
+  return moment(date).utcOffset("+0000");
 }
 
 
 function *unset(date, type) {
   date = santitizeDate(date || 'last saturday');
-  var file_date =  date.format("{yyyy}-{MM}-{dd}_{HH}.{mm}.{ss}.000Z");
+  var file_date =  get_file_date(date);
   yield mark_exported.run(`vmd_export/${type}_${file_date}`, undefined, true);//unset
 }
 
-function tar_filename(file_date, type) {
+function get_file_date(date) {
+  return date.utcOffset("+0000").format("YYYY-MM-DD_HH.mm.SS.000\\Z");
+}
+
+function export_dir(date, type) {
+  var file_date = get_file_date(date);
+  return `vmd_export/${type}_${file_date}`;
+}
+
+function tar_filename(date, type) {
+  var file_date = get_file_date(date);
   return `vmd_${type}_export_${file_date}.tar.gz`;
 }
 
@@ -74,35 +86,34 @@ function tar_filename(file_date, type) {
  */
 function *email(date) {
   date = santitizeDate(date || 'last saturday');
-  var file_date =  date.format("{yyyy}-{MM}-{dd}_{HH}.{mm}.{ss}.000Z");
-  console.log("DATE ", date, file_date);
+  
   var types = ["wp", "wc"];
   var files = [];
   for(var i = 0; i < types.length; i++) {
     var type = types[i];
-    var filename = tar_filename(file_date, type);
-    var file_names =  yield fs.readdirAsync(`vmd_export/${type}_${file_date}`);
+    var filename = tar_filename(date, type);
+    var file_names =  yield fs.readdirAsync(export_dir(date, type));
 
     files.push({
       type: {wp: "WorkPacket", wc: "WorkComplete"}[type],
       name: filename,
-      date: date.utc().format("{M}/{d}/{yyyy}"),
+      date: date.format("M/D/YYYY"),
       file_count: file_names.length,
       link: `http://file.dispatchr.co/${filename}`    
     });
   }
   
   var email_info = {
-    start_date: Date.create(date).rewind('1 week').utc().format("{M}/{d}"),
-    end_date: date.utc().format("{M}/{d}"),
+    start_date: moment(date.toDate().rewind('1 week')).format("M/D"),//moment format
+    end_date: date.format("M/D"), //moment format
     files: files
   };
-  console.log("EMAIL", email_info);
+  
   return yield gen_email({
-                            to: DISTRIBUTION_LIST, 
-                            from: 'Dispatchr <no-reply@dispatchr.com>', 
-                            replyTo: 'Gabriel Littman <gabe@dispatchr.com>', 
-                            template: 'vmd_export', 
+                            to: DISTRIBUTION_LIST,
+                            from: 'Dispatchr <no-reply@dispatchr.com>',
+                            replyTo: 'Gabriel Littman <gabe@dispatchr.com>',
+                            template: 'vmd_export',
                             values: email_info
                         });
 }
@@ -112,27 +123,26 @@ function *email(date) {
  */
 function *upload(date, type) {
   date = santitizeDate(date || 'last saturday');
-  var file_date =  date.format("{yyyy}-{MM}-{dd}_{HH}.{mm}.{ss}.000Z");  
-  var filename = tar_filename(file_date, type);
+  var filename = tar_filename(date, type);
   var file = fs.createReadStream(filename);
   yield s3.upload(S3_BUCKET, filename, file);  
 }
 
 /**
  * tar and zip up files
- * @param {String} file_date a date formated for use as  a file name
+ * @param {String} date 
  * @param {String} type the type of export "wp" (work packet) or "wc" Work Compelete
  */
-function *tar(file_date, type) {
-  var ouput_file = tar_filename(file_date, type);
+function *tar(date, type) {
+  var ouput_file = tar_filename(date, type);
   assert(!fs.existsSync(ouput_file), "Output File Exists: "+ouput_file);
-  var t = spawn('tar', ['-zcvf', ouput_file,`vmd_export/${type}_${file_date}`]);
-  console.log('[spawn] childProcess.pid: ', t.childProcess.pid);
+  var t = spawn('tar', ['-zcvf', ouput_file, export_dir(date, type)]);
+  console.log('[tar] pid: ', t.childProcess.pid);
   t.childProcess.stdout.on('data', function (data) {
-      console.log('[spawn] stdout: ', data.toString());
+      console.log('[tar] stdout: ', data.toString());
   });
   t.childProcess.stderr.on('data', function (data) {
-      console.log('[spawn] stderr: ', data.toString());
+      console.log('[tar] stderr: ', data.toString());
   });
   yield t;
 }
