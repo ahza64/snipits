a59907d6f754c57784fbf8def572687e06545380
haider <hussu2001@gmail.com>
2017-01-03 16:27:59 -0800

    DENG2:: added ingestion_config test

--- internal_admin/api/tests/unit/data/config/ingestion_configuration.js
+++ internal_admin/api/tests/unit/data/config/ingestion_configuration.js
@@ -1,12 +1,13 @@
-
+const company = require('../company/company');
+const project = require('../projects/project');
 const config = {
-  "id": 1,
   "fileType": "admin",
   "description": "123",
   "status": "active",
   "createdAt": Date.now(),
   "updatedAt": Date.now(),
-  "companyId": "123",
-  "workProjectId": "123"
-}
+  "companyId": company.id,
+  "workProjectId": project.id
+};
+
 module.exports = config;
--- internal_admin/api/tests/unit/test.config/config.js
+++ internal_admin/api/tests/unit/test.config/config.js
@@ -1,36 +1,22 @@
-
 const chai = require('chai');
 const expect = chai.expect;
 const should = chai.should();
-
 const request = require('supertest');
 const server = require('../entry');
 var agent = request(server);
-const admin = require('../data/login/admin');
 const testConfig = require('../config');
 const test_config = require('../data/config/ingestion_configuration');
-
-
+const admin = require('../data/login/admin');
+const company = require('../data/company/company');
+const project = require('../data/projects/project');
 const URL = testConfig.BASE_URL + '/config';
 const Admin = require('dsp_shared/database/model/ingestion/tables').dispatchr_admins;
+const Company = require('dsp_shared/database/model/ingestion/tables').companies;
+const Project = require('dsp_shared/database/model/ingestion/tables').work_projects;
+require('../helper');
+var savedConfigId;
 
 describe('Test for "config ingestion" methods', function () {
-console.log("-------------------", test_config);
-  before(function(done){
-      Admin.create(admin).then(() => {
-        done();
-      });
-  });
-
-  after(function(done){
-    Admin.destroy({
-      where: {
-        email: admin.email
-      }
-    }).then(() => {
-      done();
-    });
-  });
 
   it('logs in admin', done => {
     agent
@@ -44,20 +30,64 @@ console.log("-------------------", test_config);
       done();
     });
   });
-
-  it('post configuration', () => {
-    console.log("inserting config with name: " + test_config.description);
+  it('save configuration', done => {
     agent
     .post(testConfig.BASE_URL + '/config')
     .send(test_config)
     .set('Cookie', cookie)
     .end(function (err, res) {
       if(err){
-        console.error(err);
+        console.error('-------------', err);
       }
-      console.log('result of post', res.body);
       res.status.should.not.equal(404);
+      res.body.companyId.should.equal(company.id);
+      savedConfigId = res.body.id;
       done();
-    })
+    });
+  });
+  it('update configuration', done => {
+    var new_config = test_config;
+    new_config.id = savedConfigId;
+    new_config.description = "321";
+    agent
+    .post(testConfig.BASE_URL + '/config')
+    .send(new_config)
+    .set('Cookie', cookie)
+    .end(function (err, res) {
+      if(err){
+        console.error('-------------', err);
+      }
+      res.status.should.not.equal(404);
+      res.body.description.should.equal(new_config.description);
+      res.body.id.should.equal(savedConfigId);
+      done();
+    });
+  });
+  it('should get config', done => {
+    agent
+    .get(testConfig.BASE_URL + '/configs/' + project.id)
+    .set('Cookie', cookie)
+    .end(function (err, res) {
+      if(err){
+        console.error('-------------', err);
+      } else {
+        res.body[0].description.should.equal("321");
+        done();
+      }
+    });
+  });
+  it('should delete config', done => {
+    agent
+    .delete(URL + '/' + savedConfigId)
+    .set('Cookie', cookie)
+    .expect(200)
+    .end(function (err, res) {
+      if(err){
+        console.error(err);
+      } else {
+        res.body.should.equal(1);
+      }
+      done();
+    });
   });
 });
