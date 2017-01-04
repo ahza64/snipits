8cc36111089a6543b62226e9a99d0c7f1a31cb9c
haider <hussu2001@gmail.com>
2017-01-03 16:46:17 -0800

    DENG2:: added helper.js ingestion data

---
+++ internal_admin/api/tests/unit/data/ingestion/history.js
@@ -0,0 +1,5 @@
+var test_history = {
+  email: "abc@test.com",
+  action: "sample description",
+  ingestionFileId: 1, //MUST be a valid id
+};
---
+++ internal_admin/api/tests/unit/data/ingestion/ingestion_file.js
@@ -0,0 +1,7 @@
+var test_ingestion = {
+  "ingestionId": 10,
+  "ingested": false,
+  "description": "FAAAACK"
+};
+
+module.exports = test_ingestion;
