634c7edfb146b64c5a4a00e7d7db0ebabe4b40cd
Joe King <formichi3@gmail.com>
2017-01-10 14:31:42 -0800

    DENG2:: forgot to add test file in last commit

---
+++ ingestion_interface/api/tests/unit/test.upload/upload.js
@@ -0,0 +1,48 @@
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('supertest');
+const testConfig = require('../config');
+const config = require('../data/config/config')
+const ingestion_file = require('../data/ingestion/ingestion_file')
+const server = require('../entry');
+var   agent = request(server);
+const get_url = '/check/same' //followed by /:configId/:fileName
+require('../data/data_initializers/ingestion_file_init');
+//
+const user = require('../data/login/user');
+
+var cookie;
+
+
+describe('Test uploading files', () =>{
+	
+    it('Should log in', done => {
+      agent
+      .post(testConfig.BASE_URL + '/login')
+      .send({ email: user.email, password: '123'})
+      .end( (err, res) => {
+        cookie = res.header['set-cookie'].map(function(r) {
+          return r.replace('; path=/; httponly', '');
+        }).join('; ');
+        expect(err).to.equal(null);
+        done();
+      })
+    });
+
+    
+	
+	it('should check if file exists', (done) => {
+		agent
+		.get(testConfig.BASE_URL + get_url + "/" + config.id + "/" + ingestion_file.customerFileName)
+		.set('Cookie', cookie)
+	    .end( (err, res) => {
+	      if(err){
+	        console.log(err);
+	      } else {
+	        expect(res.body).to.be.true;
+			  done();
+	      }
+	      });	   
+	});	
+});
+
