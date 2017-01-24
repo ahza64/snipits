ed29e81d0ed6a116046f65a1ed1e652571d3f5ab
ahza64 <ahza64@yahoo.com>
2017-01-24 14:13:13 -0800

    DENG2 :: import search component into upload page

--- ingestion_interface/app/src/components/upload/upload.jsx
+++ ingestion_interface/app/src/components/upload/upload.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 // Components
 import DefaultNavbar from '../navbar/defaultNavbar';
 import UploadZone from '../upload/uploadZone';
+import Search from '../find/findMain';
 
 // Styles
 import Row from 'react-bootstrap/lib/Row';
@@ -18,6 +19,7 @@ export default class Upload extends React.Component {
     return (
       <div>
         <Row><DefaultNavbar /></Row>
+        <Row><Search /></Row>
         <Row><UploadZone /></Row>
       </div>
     );
