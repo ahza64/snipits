efab1a3558ea8f905411d7fd5be681ace76f3b61
haider <hussu2001@gmail.com>
2017-02-15 13:36:58 -0800

    DENG2-2897 :: added PATCH route for schema field

--- internal_admin/api/router/schema.js
+++ internal_admin/api/router/schema.js
@@ -5,7 +5,7 @@ const permissions = require('./permissions');
 // App
 const app = koa();
 const _ = require('underscore');
-
+const diff = require('deep-diff').diff;
 // Constants
 const ACTIVE = 'active';
 const INACTIVE = 'inactive';
@@ -13,6 +13,7 @@ const INACTIVE = 'inactive';
 // Collection
 const QowSchemas = require('dsp_shared/database/model/ingestion/tables').qow_schemas;
 const QowFields = require('dsp_shared/database/model/ingestion/tables').qow_fields;
+
 // Get all user schemas
 router.get(
   '/schemas/:projectId',
@@ -100,38 +101,63 @@ router.get('/schema/:schemaId', function* () {
   }
 });
 
+  router.patch('/schemaField/:schemaId', function* () {
+    var body = this.request.body;
+    var fieldId = body.id;
+    try {
+      var changes = {};
+      var originalField = yield QowFields.findOne( {
+        where: {
+          id: fieldId
+         }
+      });
+      originalField = originalField.dataValues;
+      for (var key in body) {
+        if(body[key] !== originalField[key]) {
+          console.log(changes[key], body[key]);
+          changes[key] = body[key];
+        }
+      }
+      this.body = yield QowFields.update(changes, {
+        where: {
+          id: fieldId
+        }
+      });
+      console.log("changes", changes);
+    } catch (e) {
+      console.error(e);
+    }
+
+  });
+
   router.put('/schemaField/:schemaId', function* () {
     try{
       var schemaId = this.params.schemaId;
       var body    = this.request.body;
       var name    = body.name;
-      var fieldId = body.id;
-      var version = body.version || 0;
-      var status  = body.status || true;
+      // var fieldId = body.id;
       var type    = body.type;
+      var status  = body.status || true;
       var required= body.required || true;
       if (permissions.has(this.req.user, this.req.user.companyId)) {
         var field = {
           qowSchemaId: schemaId,
           type: type,
           required: required,
-          version: version + 1,
+          version: 1,
           createdAt: Date.now(),
           updatedAt: Date.now(),
           status: status
         };
         var targetSchema = yield QowSchemas.findOne({ id : schemaId});
-
         if (!targetSchema){
           throw(404);
         }
-
-        var f = yield QowFields.create(field);
-        this.body = f;
+        this.body = yield QowFields.create(field);
       }
-  } catch(err){
+  } catch(err) {
     console.error(err);
-  }
+    }
   });
 app.use(router.routes());
 
