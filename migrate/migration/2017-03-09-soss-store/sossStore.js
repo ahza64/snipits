785658cfb5b11752a503b7d29afed27dcd438bb8
Hassu <hussu2001@gmail.com>
2017-03-09 13:25:26 -0800

    s

--- migrate/migration/2017-03-09-soss-store/sossStore.js
+++ migrate/migration/2017-03-09-soss-store/sossStore.js
@@ -1,6 +1,17 @@
-var mongosse = require('mogoose');
-var component = require('outage_layer.json');
+var _ = require('underscore');
+var mongoose = require('mongoose');
+var component = require('./outage_layer.json');
+// var Schema = mongoose.
 
+var result; 
 
-var schema = new mongoose.schema({
+var res = _.map(component, function (value, key) {
+  return {key, value};
+
 });
+
+console.log("RES==>", res);
+
+//
+// var schema = new mongoose.Schema({
+// });
