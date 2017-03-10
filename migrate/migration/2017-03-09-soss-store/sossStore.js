1f64eb84e696ddf408d30d205b7a794f6ed26408
Hassu <hussu2001@gmail.com>
2017-03-09 18:34:03 -0800

    JSON to MongoDB migration working, can accept variadric args either by file or path

--- migrate/migration/2017-03-09-soss-store/sossStore.js
+++ migrate/migration/2017-03-09-soss-store/sossStore.js
@@ -1,46 +1,46 @@
+const path = require('path');
 var _ = require('underscore');
 var mongoose = require('mongoose');
-var file = require('./outage_layer.json');
 
 var connection = mongoose.connect('mongodb://localhost:27017/migrate');
 var db = mongoose.connection;
+var sType = mongoose.Schema.Types;
+var mTypes = {
+  "string" : sType.String,
+  "number" : sType.Number,
+  "date"   : sType.Date,
+  "boolean": sType.Boolean,
+  "object" : sType.Object,
+  "array"  : sType.Array
+};
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("Database connected");
 });
 
-var fileName = process.argv[2];
-var result = {};
-var schema = mongoose.Schema.Types;
-var mTypes = {
-  "string" : schema.String,
-  "number" : schema.Number,
-  "date"   : schema.Date,
-  "boolean": schema.Boolean,
-  "object" : schema.Object,
-  "array"  : schema.Array
-};
+for (var i = 2; i < process.argv.length; i++) {
+  var filePath = './' + process.argv[i];
+  migrateToMongo(path.resolve(filePath));
+}
 
-_.each(file, function (value, key, {}) {
-  var type = typeof value;
-  console.log(key, value, type);
-  result[key] = type;
-});
-var newSchema = new mongoose.Schema(result);
+function migrateToMongo(filePath) {
+  var result = {};
+  var file = require(filePath);
+  _.each(file, (value, key, {}) => {
+    var type = typeof value;
+    result[key] = type;
+  });
+  var newSchema = new mongoose.Schema(result);
 
-_.each(file, function (value, key, {}) {
-  result[key] = value;
-});
-// var model = connection.model(fileName, newSchema);
-var Model = mongoose.model(file.name, newSchema);
-// var connect = connection.model("schema", newSchema);
-
-var saveSchema = new Model(result);
-saveSchema.save(function (err, newSchema) {
-  if (err) return console.error(err);
-  console.log("added to database");
-});
-
-
-// var schema = new mongoose.Schema({
-// });
+  _.each(file, (value, key, {}) => {
+     result[key] = value
+   });
+  var Model = mongoose.model(file.name, newSchema);
+  var saveSchema = new Model(result);
+  console.log(filePath);
+  saveSchema.save( (err, newSchema) => {
+    if (err) { return console.error(err); }
+    console.log("added to database");
+  });
+}
