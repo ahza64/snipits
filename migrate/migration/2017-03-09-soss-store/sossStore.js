d74266c0bfe8550bdf97030514ed97a33fe53245
Hassu <hussu2001@gmail.com>
2017-03-11 21:18:37 -0800

    Script works as intended

--- migrate/migration/2017-03-09-soss-store/sossStore.js
+++ migrate/migration/2017-03-09-soss-store/sossStore.js
@@ -1,7 +1,8 @@
-const path = require('path');
-var _ = require('underscore');
+const path= require('path');
+const fs  = require('fs');
+const _   = require('underscore');
+g=0;
 var mongoose = require('mongoose');
-
 var connection = mongoose.connect('mongodb://localhost:27017/migrate');
 var db = mongoose.connection;
 var sType = mongoose.Schema.Types;
@@ -18,29 +19,72 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("Database connected");
 });
+main();
 
-for (var i = 2; i < process.argv.length; i++) {
-  var filePath = './' + process.argv[i];
-  migrateToMongo(path.resolve(filePath));
+function main() {
+  for (var i = 2; i < process.argv.length; i++) {
+    var filePath = './' + process.argv[i];
+    console.log(filePath);
+    filePath = path.resolve(filePath);
+    migrateFile(filePath);
+
+  }
 }
 
-function migrateToMongo(filePath) {
-  var result = {};
+
+
+function migrateFolder(filePath){
+  var directoryFiles = fs.readdirSync(filePath);
+  _.each(directoryFiles, (file) => {
+    migrateFile(path.join(filePath, file));
+  });
+}
+
+function migrateFile(filePath) {
+  var Model;
+  var filePathObject = path.parse(filePath);
+  if (fs.lstatSync(filePath).isDirectory()){
+    migrateFolder(filePath);
+    return;
+  }
+  if (filePathObject.ext !== '.json') {
+    console.error("not a .json F");
+    return;
+  }
+  console.log(filePath);
   var file = require(filePath);
+  var result = {};
+
   _.each(file, (value, key, {}) => {
     var type = typeof value;
     result[key] = type;
   });
+  result.project  = mTypes['string'];
+  result.company  = mTypes['string'];
+
   var newSchema = new mongoose.Schema(result);
 
   _.each(file, (value, key, {}) => {
-     result[key] = value
-   });
-  var Model = mongoose.model(file.name, newSchema);
-  var saveSchema = new Model(result);
-  console.log(filePath);
-  saveSchema.save( (err, newSchema) => {
-    if (err) { return console.error(err); }
-    console.log("added to database");
+    result[key] = value
   });
+  result.project  = 'text';
+  result.company  = 'text';
+
+  if (mongoose.models[file.name]) {
+    Model = mongoose.model(file.name)
+  } else {
+    Model = mongoose.model(file.name, newSchema, file.name);
+  }
+
+  console.log(file.name);
+  var saveSchema = new Model(result);
+  saveSchema.save((err, newSchema) => {
+    if (err) {
+      console.error("FAILED:", err, filePath);
+    } else {
+      ++g;
+      console.log("success: ", file.name, filePath);
+    }
+  });
+
 }
