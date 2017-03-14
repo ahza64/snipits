5b2e3201e8c3744529c770a33941cfc5879c32a5
Hassu <hussu2001@gmail.com>
2017-03-14 15:30:28 -0700

    ML-479 :: wote script to extract sossComponents

--- SQ/sossQuery.js
+++ SQ/sossQuery.js
@@ -4,28 +4,57 @@ const _   = require('underscore');
 var mongoose = require('mongoose');
 var connection = mongoose.connect('mongodb://localhost:27017/migrate');
 var db = mongoose.connection.db;
+var Models = {};
 var dataFolder = '../../buildr-mp/api/soss/sossComponents';
 var collectionNames = []
+var sType = mongoose.Schema.Types;
+var mTypes = {
+  "string" : sType.String,
+  "number" : sType.Number,
+  "date"   : sType.Date,
+  "boolean": sType.Boolean,
+  "object" : sType.Object,
+  "array"  : sType.Array
+};
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log(path.resolve(dataFolder));
-
-  console.log();
   main();
 });
 
 function main() {
+  var result = {};
   var files = fs.readdirSync(dataFolder);
   readFolder(dataFolder);
   collectionNames = _.unique(collectionNames);
-  console.log("collectionNames =========", collectionNames);
-  findDocsByAppId(1);
+
+  var sossApp = findDocsByAppId(12, (res) => {
+    result = _.union(result, res);
+    console.log(result);
+   });
+
+}
+
+function ripSchema(file) {
+  var Model;
+  var schemaFormat = {};
+  var doc = {};
+  if (!mongoose.models[file.name]) {
+    _.each(file, (value, key, {}) => {
+      var type = typeof value;
+      schemaFormat[key] = type;
+    });
+    schemaFormat.appId  = mTypes['string'];
+    var newSchema = new mongoose.Schema(schemaFormat);
+    Model = mongoose.model(file.name, newSchema, file.name);
+  } else {
+    Model = mongoose.model(file.name);
+  }
+  Models[file.name] = Model;
 }
 
 function readFile(filePath){
   var absolutePath = path.resolve(filePath);
-  console.log('===============================', absolutePath);
   if( fs.lstatSync(absolutePath).isDirectory() ) {
     console.log('lstatSync');
     readFolder(filePath);
@@ -33,7 +62,8 @@ function readFile(filePath){
   }
   if( !filePath.endsWith('.json') ){ return; }
   var file = require(absolutePath);
-  collectionNames.push(file.name)
+  collectionNames.push(file.name);
+  ripSchema(file);
 }
 
 function readFolder(filePath) {
@@ -43,18 +73,18 @@ function readFolder(filePath) {
   })
 }
 
-function findDocsByAppId(id) {
-
+function findDocsByAppId(id, callback) {
+  var result = [];
   _.each(collectionNames, (name) => {
-    console.log("finding id", name);
-    var collection = db.collection(name)
-
-    collection.find({id : 48}, (err, res) => {
+    var Model = Models[name];
+    Model
+    .find({ appId : id })
+    .then( (res, err) => {
       if (err) {
-        console.error(err);
+        console.error('errr',err);
+      } else {
+        callback(res)
       }
-      console.log("res=========$$$$, ", res);
     });
-
   })
 }
