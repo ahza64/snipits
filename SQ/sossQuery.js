a05af57986900589ec82ca9d904595a09c77ef3e
Hassu <hussu2001@gmail.com>
2017-03-14 17:43:19 -0700

    ML-479 :: Script now works, basically copy/pastes json via mongodb

--- SQ/sossQuery.js
+++ SQ/sossQuery.js
@@ -5,9 +5,6 @@ const async = require('async');
 var mongoose = require('mongoose');
 var connection = mongoose.connect('mongodb://localhost:27017/migrate');
 var db = mongoose.connection.db;
-var Models = {};
-var dataFolder = '../../buildr-mp/api/soss/sossComponents';
-var collectionNames = []
 var sType = mongoose.Schema.Types;
 var mTypes = {
   "string" : sType.String,
@@ -18,45 +15,45 @@ var mTypes = {
   "array"  : sType.Array
 };
 
+var dataFolder = '../../buildr-mp/api/soss/sossComponents';
+const appId = 48;
+var Models = {};
+var collectionNames = []
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
+  console.log("connected to DB");
   main();
 });
 
 function main() {
-  var result = {};
   var files = fs.readdirSync(dataFolder);
   readFolder(dataFolder);
   collectionNames = _.unique(collectionNames);
-  console.log(collectionNames);
-  var sossApp = findDocsByAppId(48, (res) => {
-    console.log("AAAAAAAAAAAAA");
-    result = _.union(result, res);
+  var sossApp = findDocsByAppId(appId, (res) => {
     outputJSON(res);
    });
 }
 
+function readFile(filePath){
+  var absolutePath = path.resolve(filePath);
+  if( fs.lstatSync(absolutePath).isDirectory() ) {
+    readFolder(filePath);
+    return;
+  }
+  if( !filePath.endsWith('.json') ){ return; }
 
-function outputJSON(res) {
-  console.log(res);
-  var result ={};
-  async.each(res, (json) => {
-
-    json = json.toObject();
-    var pathx = dataFolder + '/output/';
-    pathx = path.resolve(pathx + '/' + json.fileName);
-    console.log("PATH111", pathx , json);
-    fs.writeFileSync(pathx, JSON.stringify(json), (err, res) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log("OK", res);
-      }
-    });
-
-  })
+  var file = require(absolutePath);
+  collectionNames.push(file.name);
+  ripSchema(file);
 }
 
+function readFolder(filePath) {
+  var files = fs.readdirSync(filePath);
+  async.each(files, (file) => {
+    readFile(path.join(filePath, file))
+  })
+}
 
 function ripSchema(file) {
   var Model;
@@ -76,27 +73,7 @@ function ripSchema(file) {
   Models[file.name] = Model;
 }
 
-function readFile(filePath){
-  var absolutePath = path.resolve(filePath);
-  if( fs.lstatSync(absolutePath).isDirectory() ) {
-    readFolder(filePath);
-    return;
-  }
-  if( !filePath.endsWith('.json') ){ return; }
-  var file = require(absolutePath);
-  collectionNames.push(file.name);
-  ripSchema(file);
-}
-
-function readFolder(filePath) {
-  var files = fs.readdirSync(filePath);
-  async.each(files, (file) => {
-    readFile(path.join(filePath, file))
-  })
-}
-
 function findDocsByAppId(id, callback) {
-  var result = [];
   async.each(collectionNames, (name) => {
     var Model = Models[name];
     Model
@@ -105,7 +82,24 @@ function findDocsByAppId(id, callback) {
       if (err) {
         console.error('errr',err);
       } else {
+        console.log(res);
         callback(res);
+      }
+    });
+  })
+}
+
+function outputJSON(res) {
+  var result ={};
+  async.each(res, (json) => {
+    json = json.toObject();
+    var pathx = path.resolve( dataFolder, 'output' , json.fileName);
+    console.log(pathx);
+    fs.writeFileSync(pathx, JSON.stringify(json), (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("OK");
       }
     });
   })
