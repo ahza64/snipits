735761bae647c3dcd7c69a4da145320689f3b97a
Tejas <tejasrr19@gmail.com>
2017-03-15 14:01:37 -0700

    ML-499 :: unassignment logic

--- rest_api/route/assign/assign.js
+++ rest_api/route/assign/assign.js
@@ -9,17 +9,37 @@ var router = require('koa-router')();
 var bodyParser = require('koa-bodyparser');
 
 var assignUtils = require('./assign_util');
+var unassignUtils = require('./unassign_util');
 
 router.post('/assign', function *assign(next) {
   var crew = this.request.body.crew;
   var qows = this.request.body.qows;
   var createWO = this.request.body.createWO;
 
-  yield assignUtils.updateTrees(crew, qows);
-  yield assignUtils.updateCrew(crew);
-  yield assignUtils.createWO(crew, qows);
-  this.body = "Done";
+  try {
+    yield assignUtils.updateTrees(crew, qows);
+    yield assignUtils.updateCrew(crew);
+    yield assignUtils.createWO(crew, qows);
+    this.body = "Done";
+  } catch(e) {
+    console.error('Error assigning:', e.message);
+  }
+  yield next;
+});
 
+router.post('/unassign', function *unassign(next) {
+  var crew = this.request.body.crew;
+  var project = this.request.body.project;
+
+  try {
+    console.log('Unassigning');
+    yield unassignUtils.unassignTrees(project, crew);
+    yield unassignUtils.unassignPmd(project, crew);
+    yield unassignUtils.unassignCuf(project, crew);
+    this.body = "Done";
+  } catch(e) {
+    console.error('Error unassigning:', e.message);
+  }
   yield next;
 });
 
