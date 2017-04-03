16d8f0aec09503542ee76ee59bed4f1e39905bb7
ahza64 <ahza64@yahoo.com>
2017-04-03 11:14:11 -0700

    ML-557 :: set up ui test for taxonomy

---
+++ internal_admin/app/test/integration/mocks/data/taxonomies.js
@@ -0,0 +1,42 @@
+let date = new Date();
+
+let taxonomy = [
+  {
+    id: 1,
+    fieldName: "first taxonomy",
+    order: 1,
+    nodeType: "e",
+    keys: "3",
+    createdAt: date,
+    updatedAt: date,
+    qowSchemaId: 1,
+    workProjectId: 1,
+    companyId: 1
+  },
+  {
+    id: 2,
+    fieldName: "second taxonomy",
+    order: 2,
+    nodeType: "e",
+    keys: "3",
+    createdAt: date,
+    updatedAt: date,
+    qowSchemaId: 1,
+    workProjectId: 1,
+    companyId: 1
+  },
+  {
+    id: 3,
+    fieldName: "third taxonomy",
+    order: 3,
+    nodeType: "e",
+    keys: "3",
+    createdAt: date,
+    updatedAt: date,
+    qowSchemaId: 1,
+    workProjectId: 1,
+    companyId: 1
+  }
+];
+
+module.exports = taxonomy;
---
+++ internal_admin/app/test/integration/taxonomies/taxonomies.spec.js
@@ -0,0 +1,56 @@
+/* eslint-env mocha */
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Taxonomy from '../../../src/components/taxonomy/taxonomy';
+import { mount } from 'enzyme';
+import { assert } from 'chai';
+import { expect } from 'chai';
+
+// Mocks
+const components = require('../mocks/components');
+const database = require('../mocks/database');
+const taxonomiesAPI = require('../mocks/api/taxonomies');
+
+describe('<Taxonomy />', () => {
+
+  before(function() {
+    components.init();
+    database.init();
+  });
+
+  function mountComponent() {
+    const wrapper = mount(
+      <MuiThemeProvider>
+        <Taxonomy />
+      </MuiThemeProvider>
+    );
+    var taxonomy = wrapper.find(Taxonomy);
+    expect(taxonomy).to.exist;
+    return taxonomy;
+  }
+
+  function checkTable (companyIndex, projectIndex, schemaIndex) {
+    var component = mountComponent();
+    let company = database.data.companies[companyIndex];
+    let project = database.data.projects[projectIndex];
+    let schema = database.data.schemas[schemaIndex];
+    // component.node.handleCompanySelectChanged({}, company.id);
+    // component.node.handleProjectSelectChanged({}, project.id);
+    // component.node.handleSchemaSelectChanged({}, schema.id);
+    //
+    // let text = component.text();
+    //
+    // let taxonomies = taxonomiesAPI.getTaxonomies(schema.id);
+    // taxonomies.forEach(function(c) {
+    //   assert.isTrue(text.includes(c.fieldName), `${c.fieldName} should be displayed in the table`);
+    // });
+    //
+    // let badge = component.find(Badge);
+    // assert.equal(badge.text(), taxonomies.length, `total configs found should be ${taxonomies.length}`);
+  }
+
+  it('checks taxonomy table', () => {
+    checkTable(0,0,0);
+  });
+
+});
