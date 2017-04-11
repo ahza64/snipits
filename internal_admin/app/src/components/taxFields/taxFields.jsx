1cc538dcd1d8fb303b2216d64c84d3b662932109
ahza64 <ahza64@yahoo.com>
2017-04-11 12:32:57 -0700

    ML-654 :: disable add button in schema view in expected taxonomy page

--- internal_admin/app/src/components/taxFields/taxFields.jsx
+++ internal_admin/app/src/components/taxFields/taxFields.jsx
@@ -522,7 +522,7 @@ export default class TaxFields extends React.Component {
               primary={ true }
               fullWidth={ true }
               onClick={ this.handleCreateTaxField }
-              disabled={ !this.state.taxonomyId }
+              disabled={ !this.state.taxonomyId || (this.state.viewValues !== this.state.taxonomyValues)}
             />
             { this.renderCompanySelectField() }
             { this.renderProjectSelectField() }
