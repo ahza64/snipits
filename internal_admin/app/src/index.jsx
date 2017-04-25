18c5abc2e93c9c143060e3914fdd6d7369ca767b
Hassu <hussu2001@gmail.com>
2017-04-25 13:58:11 -0700

    b

--- internal_admin/app/src/index.jsx
+++ internal_admin/app/src/index.jsx
@@ -14,7 +14,6 @@ import Configurations from './components/configs/configs.jsx';
 import Ingest from './components/ingest/ingest.jsx';
 import Users from './components/users/users.jsx';
 import SchemasLayout from './components/schema/schemasLayout.jsx';
-import Schema from './components/schema/schema.jsx';
 import Taxonomy from './components/taxonomy/taxonomy.jsx';
 import TaxFields from './components/taxFields/taxFields.jsx';
 
@@ -29,7 +28,6 @@ ReactDOM.render(
       <Route onEnter={ isRouteAuthorized } path='ingest/' component={ Ingest }></Route>
       <Route onEnter={ isRouteAuthorized } path='users/' component={ Users }></Route>
       <Route onEnter={ isRouteAuthorized } path='schemas/' component={ SchemasLayout }></Route>
-      <Route onEnter={ isRouteAuthorized } path='schema/' component={ Schema }></Route>
       <Route onEnter={ isRouteAuthorized } path='taxonomy/' component={ Taxonomy }></Route>
       <Route onEnter={ isRouteAuthorized } path='taxfields/' component={ TaxFields }></Route>
     </Route>
