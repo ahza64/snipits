ad12b3108419aa55e4f21bed7d0bd6a83c46ad37
tw3rp <rahuldprabhu@gmail.com>
2017-04-28 20:00:53 +0000

    ML-737 :: fix internal admin
    Fixes:
    - production config to change build path

--- ingestion_interface/webpack.production.config.js
+++ ingestion_interface/webpack.production.config.js
@@ -1,6 +1,10 @@
 const webpack = require('webpack');
 const webpack_config = require('./webpack.config');
+var path = require('path');
+var ROOT_PATH = path.resolve(__dirname);
 
+
+webpack_config.output.path =  path.resolve(ROOT_PATH, 'app/build_prod');
 webpack_config.devServer = undefined;
 webpack_config.plugins = [
 	new webpack.DefinePlugin({
