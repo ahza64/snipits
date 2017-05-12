d8d7ed24b3d1a32f320713e2184b152bbd3f6529
Gabriel Littman <gabe@dispatchr.co>
2017-05-12 16:12:21 -0700

    an updated linting bash script

--- lint-git.sh
+++ lint-git.sh
@@ -1,18 +1,67 @@
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
-COMPARE="HEAD"; 
-if [ $1 ]
-then    
-    COMPARE=$1; 
-fi
+show_help() {
+    echo "-h -? show help"
+    echo "-f set output format"
+    echo "-o set output file"
+    echo "-c lints only files changed since git commit specified"
+    echo "-H lints only currenly uncommited files or if no files lints the last commit"
+    echo "-x auto fix errors"
+}
 
-LIST=`git diff --name-only $COMPARE | grep .*\\.js | grep -v json`
-if [ !"$LIST" ] && [ $COMPARE == "HEAD" ]
+
+# Initialize our own variables:
+output_file=""
+output_format="-f stylish"
+all=0
+fix=""
+commit=""
+while getopts "h?xf:o:c:H" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    f)  output_format="-f $OPTARG"
+        ;;
+    o)  output_file=$OPTARG
+        ;;
+    c)  commit=$OPTARG
+        ;;
+    H)  head="true"
+        ;;
+    x)  fix="--fix"
+        ;;
+    esac
+done
+
+
+if [ "$commit" ]
 then
-    COMPARE="HEAD~1"
-    LIST=`git diff --name-only $COMPARE | grep .*\\.js | grep -v json`
+    echo "COMMIT"
+    LIST=`git diff --name-only $commit | grep .*\\.js | grep -v json`
+elif [ "$head" ]
+then
+    LIST=`git diff --name-only HEAD | grep .*\\.js | grep -v json`    
+    #if compare was HEAD and nothing found then check the current commit
+    if [ !"$LIST" ] 
+    then
+        LIST=`git diff --name-only HEAD~1 | grep .*\\.js | grep -v json`
+    fi    
+else
+    LIST=". --ext js,jsx"
 fi
 
+
+    
 if [ "$LIST" ]
 then
-    node node_modules/eslint/bin/eslint.js  $LIST;
+    echo "node node_modules/eslint/bin/eslint.js  $LIST $output_format $fix"
+    if [ "$output_file" ]
+    then
+        node node_modules/eslint/bin/eslint.js  $LIST $output_format $fix > $output_file | echo "Lint failed!"
+    else
+        node node_modules/eslint/bin/eslint.js  $LIST $output_format $fix
+    fi
 fi
