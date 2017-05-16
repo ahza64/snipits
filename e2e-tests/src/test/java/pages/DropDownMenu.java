eedadbc39db45388826e322920bc092a37568b59
AdeelZahir <adeel.zahir3@gmail.com>
2017-05-16 15:29:52 +0500

    ML-1032 Added Create Project Test.

---
+++ e2e-tests/src/test/java/pages/DropDownMenu.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+import java.net.MalformedURLException;
+
+import setup.WebAppPage;
+
+/**
+ * Created by az on 5/16/17.
+ */
+
+public class DropDownMenu extends WebAppPage {
+
+    private String dropDownButton = ".//*[@class='dropdown']/a";
+    private String companiesDropDownButton = ".//*[@class='dropdown open']/descendant::a[text()='Companies']";
+    private String workProjectsDropDownButton = ".//*[@class='dropdown open']/descendant::a[text()='Work Projects']";
+    private String ingestionsDropDownButton = ".//*[@class='dropdown open']/descendant::a[text()='Ingestion Configurations']";
+    private String usersDropDownButton = ".//*[@class='dropdown open']/descendant::a[text()='Users']";
+    private String schemasDropDownButton = ".//*[@class='dropdown open']/descendant::a[text()='Schemas']";
+    private String taxonomyDropDownButton = ".//*[@class='dropdown open']/descendant::a[text()='Taxonomy']";
+    private String taxonomyValuesDropDownButton = ".//*[@class='dropdown open']/descendant::a[text()='Taxonomy Field Values']";
+    private String logoutDropDownButton = ".//*[@class='dropdown open']/descendant::a[text()='Logout']";
+
+    public CompanyPage openCompanyPage() throws MalformedURLException
+    {
+        clickOnElement(By.xpath(companiesDropDownButton));
+        return new CompanyPage();
+    }
+
+    public ProjectPage openProjectPage() throws MalformedURLException
+    {
+        clickOnElement(By.xpath(workProjectsDropDownButton));
+        return new ProjectPage();
+    }
+}
