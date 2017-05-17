a3c147ea288e99bf4a5b8478873d073c9f17419d
AdeelZahir <adeel.zahir3@gmail.com>
2017-05-17 14:24:42 +0500

    ML-1031 Test Create Company

--- e2e-tests/src/test/java/pages/CompanyPage.java
+++ e2e-tests/src/test/java/pages/CompanyPage.java
@@ -1,6 +1,8 @@
 package pages;
 
+import org.apache.commons.logging.Log;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.net.MalformedURLException;
 
@@ -13,11 +15,59 @@ import setup.WebAppPage;
 public class CompanyPage extends WebAppPage {
 
     private String addCompanyButton = ".//*[@class='col-lg-8 col-md-8 col-sm-8 col-xs-8']/descendant::span[text()='Add company']";
+    private String addCompanyFormCancelButton = ".//span[text()='Cancel']";
+    private String addCompanyFormConfirmButton = ".//span[text()='Confirm']";
+    private String addCompanyNameField = ".//label[text()='Company Name']/following-sibling::input";
+    private String companyTable = ".//*[@class='row']";
 
     CompanyPage() throws MalformedURLException
     {
         waitForVisible(By.xpath(addCompanyButton));
         waitForPageLoadComplete();
         holdOnForACoupleOfSec();
+    }
+
+    private void waitForAddCompanyFormToDisplay()
+    {
+        try {
+            waitForVisible(By.xpath(addCompanyFormCancelButton));
+            LOGGER.info("Add Company Form is displayed");
+        } catch (MalformedURLException e) {
+            LOGGER.info("Add Company Form is not displayed");
+        }
+    }
+
+    public void addNewCompany(int companyNamePostFix)
+    {
+        clickOnElement(By.xpath(addCompanyButton));
+        waitForAddCompanyFormToDisplay();
+        driver.findElement(By.xpath(addCompanyNameField)).sendKeys("Company" + companyNamePostFix);
+        clickOnElement(By.xpath(addCompanyFormConfirmButton));
+    }
+
+    public boolean verifyNewCompanyIsAdded(int companyNamePostFix)
+    {
+        boolean isNewCompanyAdded = false;
+
+        if(isElementPresentAndDisplayedByLocator(By.xpath(companyTable)))
+        {
+            int totalRowsInCompanyTable = driver.findElements(By.xpath(".//*[@class='row']/descendant::tr")).size();
+            LOGGER.info(String.valueOf(totalRowsInCompanyTable));
+            String companyNameDisplayed;
+
+            for(int i=2; i<totalRowsInCompanyTable+2; i++)
+            {
+                companyNameDisplayed = driver.findElement(By.xpath(".//*[@class='row']/descendant::tr[" + i + "]/td[2]")).getText();
+                LOGGER.info(companyNameDisplayed);
+                if(companyNameDisplayed.contentEquals("Company" + companyNamePostFix))
+                {
+                    LOGGER.info("Company Added is Found");
+                    isNewCompanyAdded = true;
+                    break;
+                }
+            }
+        }
+
+        return isNewCompanyAdded;
     }
 }
