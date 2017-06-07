b0a1c53b58071fcb62abb0112e184436b35ac3b8
AdeelZahir <adeel.zahir3@gmail.com>
2017-06-07 14:08:23 +0500

    ML-1056 Verfiy Login Test.

--- e2e-tests/src/test/java/pagesIngestionInterface/UploadPage.java
+++ e2e-tests/src/test/java/pagesIngestionInterface/UploadPage.java
@@ -13,11 +13,29 @@ import setup.WebAppPage;
 public class UploadPage extends WebAppPage
 {
     private String dropZone = ".//div[@class='dropzone']";
+    private String heatMapContainer = ".//div[@class='highcharts-container ']";
+    private String selectProjectButton = ".//div[text()='Choose Project']";
+    private String selectConfigButton = ".//div[text()='Choose Config']";
+    private String selectProjectDropDown = ".//*[@style='padding: 16px 0px; display: table-cell; user-select: none; width: 192px;']";
+    private String selectConfigDropDown = ".//*[@style='padding: 16px 0px; display: table-cell; user-select: none; width: 192px;']";
 
     UploadPage() throws MalformedURLException
     {
         waitForVisible(By.xpath(dropZone));
+        waitForVisible(By.xpath(heatMapContainer));
+        waitForVisible(By.xpath(selectProjectButton));
+        waitForVisible(By.xpath(selectConfigButton));
         waitForPageLoadComplete();
         holdOnForACoupleOfSec();
+    }
+
+    public void selectProject(int namePostFix) throws MalformedURLException
+    {
+        selectEntity("Project", namePostFix, selectProjectButton, selectProjectDropDown);
+    }
+
+    public void selectConfig(int namePostFix) throws MalformedURLException
+    {
+        selectEntity("Config", namePostFix, selectConfigButton, selectConfigDropDown);
     }
 }
